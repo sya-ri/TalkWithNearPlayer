9c085892d23631fe9e734712a6d6f0007f4194d7
sya-ri <sya79lua@gmail.com>
2021-02-23 19:28:36 +0900

    サーバーを起動してから一度しかDMに対して認証失敗のメッセージを送信しないようにした #12 close #12

--- src/main/kotlin/com/github/syari/plugin/talk/with/near/player/discord/DiscordEventListener.kt
+++ src/main/kotlin/com/github/syari/plugin/talk/with/near/player/discord/DiscordEventListener.kt
@@ -5,14 +5,19 @@ import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 object DiscordEventListener : ListenerAdapter() {
+    private val alreadyFailureMessage = mutableSetOf<Long>()
+
     override fun onPrivateMessageReceived(event: PrivateMessageReceivedEvent) {
         if (event.author.isBot) return
-        val authPlayer = DiscordMember.auth(event.message.contentRaw, event.author.idLong)
+        val authorId = event.author.idLong
+        val authPlayer = DiscordMember.auth(event.message.contentRaw, authorId)
         if (authPlayer != null) {
             authPlayer.player?.sendMessage(templateMessage("&6${event.author.asTag} &fと連携しました"))
             event.channel.sendMessage("認証に成功しました")
         } else {
-            event.channel.sendMessage("認証に失敗しました")
+            if (alreadyFailureMessage.contains(authorId)) return
+            alreadyFailureMessage.add(authorId)
+            event.channel.sendMessage("`/twnp connect` というコマンドをサーバー内で実行して認証コードを取得してください")
         }.complete()
     }
 }
